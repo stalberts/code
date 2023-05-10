3ad5aa510507bd450ae79abc5120af6914a695b5
stalberts <133041753+stalberts@users.noreply.github.com>
2023-05-10 11:10:44 +0530

    Create index.php

---
+++ quiz/index.php
@@ -0,0 +1,29 @@
+<html>
+<head>
+<title>quiz</title>
+<link href="style.css" type="text/css" rel="stylesheet"/>
+</head>
+<body>
+<div id="container">
+<div id="header">
+<h1><b><font size=20>QUIZ CARD</font></h1>
+</div>
+<div id="navigation">
+<ul>
+<li><a href="index.php"><b>HOME</a><strong></strong></li>
+<li><a href="quiz.php"><b>QUIZ</a><strong></strong></li>
+</ul>
+</div>
+<div id="content">
+</div>
+<div id="image">
+<img src="imgfoot.jpg" alt=""/>
+</div>
+</div>
+<div id="footer">
+All right reserved </font>
+</div>
+</div>
+</div>
+</body>
+</html>
