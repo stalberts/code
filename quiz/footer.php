8d9c119bdcbcd5f0784def9370943eaf1687e41d
stalberts <133041753+stalberts@users.noreply.github.com>
2023-05-10 11:11:33 +0530

    Create footer.php

---
+++ quiz/footer.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+<title>QUIZ CARD</title>
+<link href="style.css" type="text/css" rel="stylesheet"/>
+</head>
+<body> 
+<div id="image">
+<img src="imgfoot.jpg" alt=""/>
+</div> 
+<div id="footer">
+All right reserved 
+</div>
+</body>
+</html>
