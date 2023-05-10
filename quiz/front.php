2da64d389a8987c61870f468aa28c36a813176df
stalberts <133041753+stalberts@users.noreply.github.com>
2023-05-10 11:11:05 +0530

    Create front.php

---
+++ quiz/front.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+<title>QUIZ CARD</title>
+<link href="style.css" type="text/css" rel="stylesheet"/>
+</head>
+<body>
+ <div id="container">
+ <div id="header">
+<h1><b><font size=20>QUIZ CARD</font></h1>
+ </div>
+ <div id="navigation">
+ <ul>
+<li><a 
+href="index.php"><b>HOME</a><strong></strong></li>
+<li><a 
+href="quiz.php"><b>QUIZ</a><strong></strong></li>
+ </ul>
+</div>
+</div>
+</body>
+</html>
